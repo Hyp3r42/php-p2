71cb40b0e023bb8d6498efa8d66729de79f9c728
HYp3r42 <9021951@student.tcrmbo.nl>
2023-11-22 10:03:14 +0100

    aanpassing

---
+++ opdracht3.php
@@ -0,0 +1,13 @@
+<?php
+
+$string1 = "Ik ben";
+$string2 = "Talha!";
+
+echo "<p>$string1 $string2</p>";
+
+$nummer1 = 3534;
+$nummer2 = 354;
+
+print "<p>$nummer1</p>";
+print "<p>$nummer2</p>";
+?>
