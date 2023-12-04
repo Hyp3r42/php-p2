581c61215b859615e56abe0464179a50f3be1dc6
HYp3r42 <9021951@student.tcrmbo.nl>
2023-12-04 12:54:28 +0100

    aanpassing

---
+++ .php
@@ -0,0 +1,19 @@
+<?php
+   //auteur: Zoheb
+   //functie: voorbeeld operatoren blz 4.36
+
+   $getal = 7000;
+   // optellen 1000 + 2000
+   $uitkomst = 1000 + 2000;
+   echo "Uitkomst = $uitkomst";
+
+   $uitkomst = $uitkomst % 10;
+
+ 
+
+
+
+
+
+
+?>
