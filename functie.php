60f54b70f4c025c9cab628adf185f5afeca34cab
HYp3r42 <9021951@student.tcrmbo.nl>
2023-12-12 12:57:22 +0100

    aanpassing

---
+++ functie.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+
+
+a
+g
+s
+e
+l
+p
+h
+p
+function printvertikaal ($tekst) {
+    // code van de fucntie
+    echo "$tekst<br>";
+
+    // 2e tekst wil printen
+    echo $tekst [0] . "<br>";
+    echo $tekst [1] . "<br>";
+    echo $tekst [2] . "<br>";
+    echo $tekst [3] . "<br>";
+    // bepaal lengte van de tekst
+    strlen($string);
+}
+
+//main 
+
+printvertikaal(agse);
+
+printvertikaal(lphp);
+?>
