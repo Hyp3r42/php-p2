4c00d5331437219a72278ffeee880e3ff7e45576
HYp3r42 <9021951@student.tcrmbo.nl>
2023-12-20 15:22:24 +0100

    aanpassing

---
+++ opd.php
@@ -0,0 +1,10 @@
+<?php
+$getal1 = 10;
+$getal2 = 36;
+$antwoord = $getal1 * $getal2
+echo "$antwoord"
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
