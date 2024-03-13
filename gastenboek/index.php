d3cfa41f2b36774dbfeb9fb2409bc7b201bb23bf
HYp3r42 <9021951@student.tcrmbo.nl>
2024-03-13 14:17:03 +0100

    .

--- gastenboek/index.php
+++ gastenboek/index.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Opdracht 6 hoofdstuk 9: gastenboek</title>
-</head>
-<body>
-    <h1>Opdracht 6 hoofdstuk 9: gastenboek</h1>
+<h1>Opdracht 6 hoofdstuk 9: gastenboek</h1>
     <form method="post" action="">
         Naam: <input type="text" name="naam" id="naam"></input></br></br>
         Bericht: <textarea type="text" name="bericht" id="bericht"></textarea></br></br>
         <input type="submit" name="knop" id="knop">
     </form>
+
+
     <?php
     include "connectpdo.php";
 
@@ -19,16 +13,16 @@
         // INSERT query uitvoeren
         $stmt = $conn->prepare("INSERT INTO berichten (naam, bericht, datumtijd) 
         VALUES (:naam, :bericht, :datumtijd)");
-        $stmt->bindParam(":naam", $naam);
-        $stmt->bindParam(":bericht", $bericht);
-        $stmt->bindParam(":datumtijd", $datumtijd);
+        $stmt->bindParam(':naam', $naam);
+        $stmt->bindParam(':bericht', $bericht);
+        $stmt->bindParam(':datumtijd', $datumtijd);
 
         // insert rij
         if (isset($_REQUEST['naam']))
          {
             $naam = $_POST['naam'];
             $bericht = $_POST['bericht'];
-            $datumtijd = date('d-m-Y H:i:s');
+            $datumtijd = date('d-m-Y');
             $stmt->execute();
         
         // terugsturen naar de hoofdpagina
@@ -36,7 +30,7 @@
          }}
          catch(PDOException $e)
          {
-            echo "Error: " . $e->getMassage()
+            echo "Error: " . $e->getMassage();
          }
 
          // Berichten ophalen
@@ -49,9 +43,8 @@
             echo $row ['naam']." ";
             echo $row ['bericht']." ";
         // echo "<a href='pdoupdate.php?id=$row[id]'>Bewerken</a>";
-        echo "<a href='VerwijderBericht.php?id"
+        echo "<a href='VerwijderBericht.php?id=$row[id]'>Verwijderen</a>";
+        echo "</br>";
          }
-
-
-
+         $conn = null;
     ?>
