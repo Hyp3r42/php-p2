44c2d2d79123e8eb8776b7f372e6a4a49818b112
HYp3r42 <9021951@student.tcrmbo.nl>
2024-03-22 12:58:53 +0100

    .

--- poll/index.php
+++ poll/index.php
@@ -1,3 +1,4 @@
+<h1>Poll</h1>
 <?php
 // Inclusief de database connectie bestand
 include 'connect.php';
@@ -23,7 +24,7 @@ if (isset($_POST['submit'])) {
         $stmt = $conn->prepare("INSERT INTO poll (question_id, choice, votes) VALUES (?, ?, 1)");
         $stmt->execute([$questionId, $choice]);
     }
-
+}
     // Haal alle vragen op uit de database
     $stmt = $conn->prepare("SELECT * FROM vraag_en_opties");
     $stmt->execute();
@@ -33,52 +34,26 @@ if (isset($_POST['submit'])) {
     foreach ($questions as $question) {
         echo $question["vraag"] . "<br/>";
         echo '<form action="" method="post">';
-
-        // Itereer door elk mogelijk antwoord en maak een radiobutton voor elk
+        // Itereer door elk mogelijk antwoord en maak een radiobutton
         for ($i = 1; $i <= 4; $i++) {
             $answer = $question["antwoord" . $i];
             if (!empty($answer)) {
-                echo '<input type="radio" name="choice" value="' . $i . '"> ' . htmlspecialchars($answer) . '<br/>';
+                echo '<input type="radio" name="choice" value="' . $i . '">' . $answer.'<br/>';
             }
         }
 
-        // Verberg de vraag ID in een hidden input
-        echo '<input type="hidden" name="questionId" value="' . $question["id"] . '">';
-
-        // Toon een knop om de stem in te dienen
-        echo '<input type="submit" name="submit" value="Stem">';
-        echo '</form>';
-        echo "<hr/>";
+        // Voeg een verborgen ve,d toe om de vraag ID mee te sturen
+        echo '<input type="hidden" name="questionId" value="' . $question['id'] . '">';
+        // Submit knop voor het stemmen
+        echo '<input type="submit" name="submit">Submit</button>';
+        echo '</form><br/>';
     }
-} else {
-    // Als het formulier nog niet is ingediend, haal alle vragen op uit de database
-    $stmt = $conn->prepare("SELECT * FROM vraag_en_opties");
-    $stmt->execute();
-    $questions = $stmt->fetchAll();
 
-    // Voor elke vraag, toon de vraag en de antwoordopties
-    foreach ($questions as $question) {
-        echo $question["vraag"] . "<br/>";
-        echo '<form action="" method="post">';
+    // Links voor het beheren van vragen en het opnieuw laden van de poll
+    echo '<p><a href="manage_questions.php">Vragen beheren</a></p>';
 
-        // Itereer door elk mogelijk antwoord en maak een radiobutton voor elk
-        for ($i = 1; $i <= 4; $i++) {
-            $answer = $question["antwoord" . $i];
-            if (!empty($answer)) {
-                echo '<input type="radio" name="choice" value="' . $i . '"> ' . htmlspecialchars($answer) . '<br/>';
-            }
-        }
 
-        // Verberg de vraag ID in een hidden input
-        echo '<input type="hidden" name="questionId" value="' . $question["id"] . '">';
-
-        // Toon een knop om de stem in te dienen
-        echo '<input type="submit" name="submit" value="Stem">';
-        echo '</form>';
-       echo "<hr/>";
-    }
-}
-
-// Sluit de Databas
-$conn = null;
-?>
+    // Inclusief het resultaten script om de huidige stand van de poll te tonen
+    echo "<h1>Tussenstand</h1>";
+    Include "results.php";
+    ?>
